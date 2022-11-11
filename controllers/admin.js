ad71bc32720cd06e54b9d70a42eb24e454c84901
shashank4214@gmail.com <shashank-gr>
2022-11-11 13:42:27 +0530

    added Edit & delete functionality to adminProducts

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,22 +4,45 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
+  const prodId = req.body.productId;
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
+  console.log(req.body);
+  if (prodId) {
+    const product = new Product(prodId, title, imageUrl, description, price);
+    product.save();
+    res.redirect("/");
+  } else {
+    const product = new Product(null, title, imageUrl, description, price);
+    product.save();
+    res.redirect("/");
+  }
 };
 
+exports.postEditProduct = (req, res) => {
+  const prodId = req.params.productId;
+  console.log(req.query.edit);
+  Product.fetchProductById(prodId, (product) => {
+    res.render("admin/edit-product", {
+      prod: product,
+      path: "",
+      pageTitle: "Edit Product",
+    });
+  });
+};
+
+exports.postDeleteProduct = (req, res) => {
+  const prodId = req.params.productId;
+  Product.deleteProductByid(prodId, () => {
+    res.redirect("/");
+  });
+};
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("admin/products", {
--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,13 +27,26 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
-    getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+    if (this.id) {
+      getProductsFromFile((products) => {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id == this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
       });
-    });
+    } else {
+      this.id = Math.random().toString();
+      getProductsFromFile((products) => {
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      });
+    }
   }
 
   static fetchAll(cb) {
@@ -43,6 +57,15 @@ module.exports = class Product {
     getProductsFromFile((products) => {
       const product = products.find((p) => p.id == id);
       cb(product);
+    });
+  }
+  static deleteProductByid(id, cb) {
+    Product.fetchAll((products) => {
+      const updatedProducts = products.filter((p) => p.id != id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        console.log(err);
+        cb();
+      });
     });
   }
 };
